235283fc3da56ef638fc28d62c947b3c49e090d0
hp4 <hp4@192.168.1.8>
2015-12-27 18:24:01 +0000

    initial commit

--- wp-content/themes/wpmovies/includes/single/backdrops.php
+++ wp-content/themes/wpmovies/includes/single/backdrops.php
@@ -1,5 +1,5 @@
 <?php if($values = get_post_custom_values("imagenes")) { ?>
-<div id="owl-demo" class="hhhhhhhhhhhhhhhhh owl-carousel">
+<div id="owl-demo" class="owl-carousel">
 <?php $img = get_post_meta($post->ID, "imagenes", $single = true); backdrops ($img); ?>
 </div>
 <?php } ?>
