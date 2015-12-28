98641c7be7508165b0557d6b336d1c9c7336d417
hp4 <hp4@192.168.1.8>
2015-12-28 13:56:24 +0000

    fix stars

--- wp-content/themes/wpmovies/index.php
+++ wp-content/themes/wpmovies/index.php
@@ -35,7 +35,6 @@
 <?php while (have_posts()) : the_post(); 
 
 $post_ratings_data = get_post_custom($post->ID);
-     var_dump($post_ratings_data);die;
 if (has_post_thumbnail()) {
 $imgsrc = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'home');
 $imgsrc = $imgsrc[0];
@@ -54,7 +53,7 @@ $imgsrc = $img[0];
 <div class="imagen">
 <img src="<?php echo $imgsrc; $imgsrc = ''; ?>" alt="<?php the_title(); ?>" width="100%" height="100%" />
 <a href="<?php the_permalink() ?>"><span class="player"></span></a>
-<div class="imdb"><span class="icon-grade"></span> <?php _e('average', 'wp-postratings'); ?></div>
+<div class="imdb"><span class="icon-grade"></span> <?php echo $post_ratings_data->ratings_average ?></div>
 </div>
 <h2><?php the_title(); ?></h2>
 <?php if($mostrar = $terms = strip_tags( $terms = get_the_term_list( $post->ID, ''.$year_estreno.'' ))) {  ?><span class="year"><?php echo $mostrar; ?></span><?php } ?>
