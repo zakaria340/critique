ba4d945d868ce274d2a303707fbf62fbf7c8a7a2
Zakaria Elhariri <zakaria.elhariri@smile-maroc.com>
2016-01-06 16:14:56 +0000

    analytics

--- wp-content/plugins/wp-postratings/wp-postratings.php
+++ wp-content/plugins/wp-postratings/wp-postratings.php
@@ -1293,7 +1293,7 @@ function mine_expand_ratings_template($template, $post_data, $post_ratings_data 
     // Replace the variables
     $value = $template;
     if (strpos($template, '%RATINGS_IMAGES%') !== false) {
-        $post_ratings_images = '<tag>'.mine_get_ratings_images_vote($ratings_custom, $ratings_max, $post_ratings, $ratings_image, $post_ratings_alt_text, $insert_half).'</tag>';
+        $post_ratings_images = '<tag>'.get_ratings_images_vote($ratings_custom, $ratings_max, $post_ratings, $ratings_image, $post_ratings_alt_text, $insert_half).'</tag>';
         $value = str_replace("%RATINGS_IMAGES%", $post_ratings_images, $value);
     }
     if (strpos($template, '%RATINGS_IMAGES_VOTE%') !== false) {
