9072e72438a397ca7daf0451efdb7750ec482487
Zakaria Elhariri <zakaria.elhariri@smile-maroc.com>
2016-01-06 10:03:21 +0000

    analytics

--- wp-content/themes/wpmovies/includes/funciones/mas_votados.php
+++ wp-content/themes/wpmovies/includes/funciones/mas_votados.php
@@ -6,9 +6,7 @@
 
 <?php endif; ?> 
 <ul class="scrolling lista">
-<?php $numerado = 1; { query_posts(
-array('meta_key' => 'end_time','meta_compare' =>'>=','meta_value'=>time(),'meta_key' => 'imdbRating',
-'post__not_in' => get_option( 'sticky_posts' ),'orderby' => 'meta_value_num','showposts' => '20','order' => 'DESC'));
+<?php $numerado = 1; { query_posts( array( 'meta_key' => 'ratings_average', 'orderby' => 'meta_value_num', 'order' => 'DESC' ) );
 while ( have_posts() ) : the_post(); 
 $imdbRating = get_post_meta($post->ID, "imdbRating", $single = true); ?>
 <li class="number-<?php echo $numerado; ?>">
