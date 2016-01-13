343448544259133506b5862f12b8ee3ac0ec6774
Zakaria Elhariri <zakaria.elhariri@smile-maroc.com>
2016-01-13 08:39:18 +0000

    analytics

--- wp-content/themes/wpmovies/header.php
+++ wp-content/themes/wpmovies/header.php
@@ -12,6 +12,7 @@
 
   <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/reset.css"/>
   <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/mt.min.css"/>
+  <link rel="stylesheet" type="text/css" href="http://semantic-ui.com/dist/semantic.min.css"/>
   <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'>
